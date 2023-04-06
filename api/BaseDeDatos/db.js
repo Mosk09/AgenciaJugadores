625007ca5377ff3134ac6f2e9e6ca7a054f9df75
Mosk09 <leomosconi9@gmail.com>
2023-04-06 11:39:20 -0300

    listo

--- api/BaseDeDatos/db.js
+++ api/BaseDeDatos/db.js
@@ -3,7 +3,7 @@ import  { Sequelize } from 'sequelize';
 import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } from "../BaseDeDatos/confg.js"
 
 // Creamos una nueva instancia de Sequelize y configuramos la conexión a la base de datos.
-export const sequelize = new Sequelize(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,{
+export const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,{
   host: DB_HOST,
   dialect: 'postgres'
 })
